33bdd4bf2e20690faa85df50c8e8238520849d7d
이유빈 <dbqls2757h@naver.com>
2022-04-19 10:06:07 +0900

    test update

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from "../components/AppLayout";
+
+const Index = () => {
+    return (
+        <AppLayout>
+            <div>hi i'm yoobin❤️</div>
+        </AppLayout>
+    );
+};
+
+export default Index;
---
+++ front/pages/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from "../components/AppLayout";
+
+const Singup = () => {
+    return (
+        <AppLayout>
+            회원가입
+        </AppLayout>
+    );
+};
+
+export default Singup;
